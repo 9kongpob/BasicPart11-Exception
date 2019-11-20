56e0a5d0d57ef6107842ff3a15be684cde53baef
9kongpob <k.kunchanuan@hotmail.com>
2019-11-20 09:07:54 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BasicPart11_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            Console.Write("Input first number : ");
+            int.TryParse(Console.ReadLine(), out x);
+            Console.Write("Input second number : ");
+            int.TryParse(Console.ReadLine(), out y);
+
+            try
+            {
+
+                double z = x / y;
+
+                Console.WriteLine("Output : " + z);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Divide by Zero");
+            }
+            finally
+            {
+                Console.WriteLine("End Program.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
